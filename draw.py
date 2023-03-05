f34165397e68b22b1a913c3826d6e368c4d1a023
keyork <chengky18@icloud.com>
2023-03-05 14:22:13 +0800

    exp: test data

--- draw.py
+++ draw.py
@@ -89,5 +89,41 @@ def draw(path_mlp, path_stat):
     plt.cla()
 
 
+def draw_test():
+
+    x = [100, 500, 1000, 5000, 10000, 30000, 55000]
+    mlp_acc = [0.7247, 0.8598, 0.8838, 0.9247, 0.9521, 0.9520, 0.94549996]
+    stat_acc = [0.7643, 0.8748, 0.8971, 0.9370, 0.955, 0.9707, 0.9752]
+    mlp_loss = [0.414172, 0.147488, 0.112205, 0.056688, 0.026235, 0.008682, 0.007593]
+    stat_loss = [0.148356, 0.042105, 0.022027, 0.007350, 0.004028, 0.002041, 0.001943]
+
+    plt.figure(dpi=300)
+
+    plt.plot(x, stat_acc, "", color="r")
+    plt.plot(x, mlp_acc, "", color="b")
+
+    label = ["StatNet", "MLPNet"]
+
+    plt.legend(label, loc="upper left")
+    plt.title("Test Acc Curve")
+    plt.xlabel("Train Data Num")
+    plt.ylabel("Test Acc")
+    plt.savefig("./test_acc.jpg")
+    plt.cla()
+
+    plt.plot(x, stat_loss, "", color="r")
+    plt.plot(x, mlp_loss, "", color="b")
+
+    label = ["StatNet", "MLPNet"]
+
+    plt.legend(label, loc="upper left")
+    plt.title("Test Loss Curve")
+    plt.xlabel("Train Data Num")
+    plt.ylabel("Test Loss")
+    plt.savefig("./test_loss.jpg")
+    plt.cla()
+
+
 if __name__ == "__main__":
-    draw("mlpnet-exp2-30000.npz", "statnet-exp2-30000.npz")
+    # draw("mlpnet-exp2-30000.npz", "statnet-exp2-30000.npz")
+    draw_test()
